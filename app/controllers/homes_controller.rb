25ad1df9b4257803525eb5cc5bdf8b501c7c4e70
kk-ketan <ketan.kaushal@bluebash.co>
2023-04-03 13:40:35 +0530

    fixed ransack issue

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
     def index
         @homes=Music.all.where(published: true)
-        @ketan=@homes.ransack(params[:ketan])
-        @homes=@ketan.result(distinct: true)
+        @q=@homes.ransack(params[:q])
+        @homes=@q.result(distinct: true)
     end
 end
